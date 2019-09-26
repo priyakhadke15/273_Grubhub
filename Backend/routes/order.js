ff80fcefe004f92ddd7283640dec8c29be9e30ba
Priya Khadke <priyakhadke15@gmail.com>
2019-09-26 16:28:30 -0700

    /order backend updated for multiple items selection.

--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -49,43 +49,56 @@ router.get('/details', async function (req, res, next) {
     }
 });
 //submit the buyer's order
+/* item input in format [{"itemID":"cdf5b752-4b43-4457-adf6-81d83835bf65","quantity":"2"},
+{"itemID":"cdf5b752-4b43-4457-adf6-81d83835bf66","quantity":"1"}]
+*/
 router.post('/', async function (req, res, next) {
     var total = 0, ordertotal = 0;
-    const {
-        itemID, quantity,
-        restaurantId, deliveryAdd
-    } = req.body;
+    var randNum = uuidv4();
+    const { items, restaurantId, deliveryAdd } = req.body;
+    const itemjson = JSON.parse(items);
+
     //check if user is logged in
     if (!(req.cookies.authCookie)) {
         console.error("Unauthorised access");
         return res.status(401).json({ message: "please login to continue" });
     }
+    //check items are selected
+    if (itemjson.length == 0) {
+        console.error("No item selected");
+        return res.status(400).json({ message: "please select atleast one item" });
+    }
     var d = new Date();
     var curr_date = d.getDate();
-    var curr_month = d.getMonth();
+    var curr_month = d.getMonth() + 1;
     var curr_year = d.getFullYear();
+    var seconds = d.getSeconds();
+    var minutes = d.getMinutes();
+    var hour = d.getHours();
 
     try {
-        const user = jwt.verify(req.cookies.authCookie, jwtsecret);
-        const item = {
-            itemID
-        };
-        const { results } = await getItems(item);
-        Restresult = JSON.parse(JSON.stringify(results[0]))
-        total += Restresult.price * quantity;
-        const orderdetail = {
-            orderID: uuidv4(),
-            itemprice: Restresult.price,
-            totalprice: total,
-            itemID, quantity
-        };
-        await saveOrderDetails(orderdetail);
+        //get item price for each item and calculate order price
+        for (var i = 0; i < itemjson.length; i++) {
+            const { itemID, quantity } = itemjson[i];
+            const item = { itemID };
+            const { results } = await getItems(item);
+            Restresult = JSON.parse(JSON.stringify(results[0]))
+            total = Restresult.price * quantity;
 
-        ordertotal += orderdetail.totalprice;
+            const orderdetail = {
+                orderID: randNum,
+                itemprice: Restresult.price,
+                totalprice: total,
+                itemID, quantity
+            };
+            await saveOrderDetails(orderdetail);
+            ordertotal += total;
+        }
+        const user = jwt.verify(req.cookies.authCookie, jwtsecret);
         const order = {
-            orderID: orderdetail.orderID,
+            orderID: randNum,
             buyerId: user.id,   //buyer's userid is fetched from authcookie
-            orderDate: (curr_year + '-' + curr_month + '-' + curr_date),
+            orderDate: (curr_year + '-' + curr_month + '-' + curr_date + ' ' + hour + ':' + minutes + ':' + seconds),
             status: 'new',
             price: ordertotal,
             restaurantId, deliveryAdd
