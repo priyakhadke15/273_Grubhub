e0fd319c480db5a789ac37e3960973f1ec1821f4
Priya Khadke <priyakhadke15@gmail.com>
2019-09-28 17:32:01 -0700

    added background image for home page

--- frontendnew/src/components/SiteDescription.js
+++ frontendnew/src/components/SiteDescription.js
@@ -2,30 +2,29 @@ import React, { Component } from 'react';
 class SiteDescription extends Component {
     render() {
         return (
-            <div className="hero">
+            <div className="hero" style={{ width: "100%", backgroundColor: "rgba(255,255,255,0.5)", backgroundBlendMode: "overlay", backgroundImage: 'url("/food1.jpg")', backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
                 <div className="container">
-                    <img className="mainlogo" src="/cupcake.jpg" alt="" ></img>
                     <h1 className="site-title">GrubHub</h1>
                     <small className="site-description">How to order food ? with GrubHub its easy</small>
                 </div>
                 <div className="row">
                     <div className="col-md-4">
                         <div className="feature color-purple">
-                            <img style={{paddingTop:"0px",padding:"40px"}} className="feature-icon" src="/search.png"></img>
+                            <img style={{ paddingTop: "0px", padding: "40px" }} className="feature-icon" src="/search.png"></img>
                             <h3 className="feature-title">Where</h3>
                             <p>Browse menus from local restaurants</p>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="feature color-green">
-                            <img style={{paddingTop:"0px",padding:"40px"}} className="feature-icon" src="/hand.png"></img>
+                            <img style={{ paddingTop: "0px", padding: "40px" }} className="feature-icon" src="/hand.png"></img>
                             <h3 className="feature-title">What</h3>
                             <p>Select your favourite dish and complete the order</p>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="feature color-pink">
-                            <img style={{paddingTop:"0px",padding:"40px"}} className="feature-icon" src="/food.png"></img>
+                            <img style={{ paddingTop: "0px", padding: "40px" }} className="feature-icon" src="/food.png"></img>
                             <h3 className="feature-title">Delivery</h3>
                             <p>Thats it you are all set. The Food will be delivered to you</p>
                         </div>
