76362cac56bad1613271bea11f6d4763ad402fdf
Priya Khadke <priyakhadke15@gmail.com>
2019-10-03 10:11:06 -0700

    Cart page frontend

--- frontendnew/src/components/LandingPage/NavBar.js
+++ frontendnew/src/components/LandingPage/NavBar.js
@@ -19,13 +19,14 @@ class NavBar extends Component {
                                 {this.props.isLoggedIn && !this.props.isSeller && <li className="menu-item"><Link to="/search">Search</Link></li>}
                                 {this.props.isLoggedIn && <li className="menu-item"><Link to="/account/profile">Account</Link></li>}
                                 {this.props.isSeller && <li className="menu-item"><Link to="/menu">Menu</Link></li>}
+                                {this.props.isLoggedIn && !this.props.isSeller && <li className="menu-item"><Link to="/cart">Cart</Link></li>}
                                 {this.props.isLoggedIn && <li className="menu-item"><Link to="/login" onClick={this.props.logout.bind(this)}>Logout</Link></li>}
                                 {!this.props.isLoggedIn && <li className="menu-item"><Link to="/login">Login</Link></li>}
                             </ul>
                         </div>
                     </div >
                 </div>
-            </div>
+            </div >
         );
     }
 }
