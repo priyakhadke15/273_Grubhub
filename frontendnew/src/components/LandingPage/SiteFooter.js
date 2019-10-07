c8295a8ba5fb061b17919b97739b62413d70fd4d
Priya Khadke <priyakhadke15@gmail.com>
2019-10-06 18:23:44 -0700

    site footer is now same as navbar

--- frontendnew/src/components/LandingPage/SiteFooter.js
+++ frontendnew/src/components/LandingPage/SiteFooter.js
@@ -1,35 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './NavBar';
 
 class SiteFooter extends Component {
 
     render() {
         return (
-            <footer className="site-footer" >
-                <div className="container" >
-                    <i className="icon-cupcake logo"></i>
-                    <nav className="footer-navigation" >
-                        <a href="#">About me</a>
-                        <a href="#">My offer</a>
-                        <a href="#">Recipes</a>
-                        <a href="#">Contact</a>
-                    </nav>
-                    <div className="subscribe">
-                        <form action="#">
-                            <input type="text" placeholder="Enter your email to join newsletter..." />
-                            <input type="submit" value="Subscribe" />
-                        </form>
-                    </div>
-                    <div className="social-links">
-                        <a href="#" className="facebook"><i className="fa fa-facebook"></i></a>
-                        <a href="#" className="twitter"><i className="fa fa-twitter"></i></a>
-                        <a href="#" className="google-plus"><i className="fa fa-google-plus"></i></a>
-                        <a href="#" className="pinterest"><i className="fa fa-pinterest"></i></a>
-                    </div>
-                    <div className="colophon">
-                        <p>Copyright 2014 Company name. Designed by Themezy. All right reserved</p>
-                    </div>
-                </div>
-            </footer>
+            <Navbar />
         )
     }
 }
