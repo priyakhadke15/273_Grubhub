25b65c4ead4b1b7a892592e086c0725629c1091f
Priya Khadke <priyakhadke15@gmail.com>
2019-12-06 21:56:06 -0800

    GraphQL PoC added schema.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+const graphqlHTTP = require('express-graphql');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 const { initDb } = require('./config');
 
+const schema = require('./schema/schema');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var restaurantRouter = require('./routes/restaurant');
@@ -50,6 +52,7 @@ app.use(`/api/${apiVersion}/users`, usersRouter);
 app.use(`/api/${apiVersion}/restaurant`, restaurantRouter);
 app.use(`/api/${apiVersion}/item`, itemRouter);
 app.use(`/api/${apiVersion}/order`, orderRouter);
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -66,5 +69,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(8080, () => {
+  console.log("GraphQL server started on port 8080");
+})
 module.exports = app;
